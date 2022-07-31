31f8689d3f26aeca37351940c09ea674aca19d35
Evan Jones <evanjones125@gmail.com>
2022-07-31 16:16:13 -0400

    add lorem text file

--- ch_09/common_word.py
+++ ch_09/common_word.py
@@ -1,2 +1,21 @@
-handle = open(lorem.txt)
-print(handle)
+# def common_word(file):
+#     handle = open(file)
+#     store = []
+#     for line in handle:
+#         # words = line.split()
+#         new_list = line.split()
+#         store.append(new_list[1])
+#     return store
+
+# print(common_word('lorem.txt'))
+
+
+handle = open('lorem.txt')
+
+# extract all emails and store in a lis
+# store = []
+for line in handle:
+    line = line.rstrip()
+    print(line)
+
+# print(store)
