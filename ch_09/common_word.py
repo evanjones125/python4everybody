8ab36cd9ecb1521fd0d22ad4d40b211dc046114a
Evan Jones <evanjones125@gmail.com>
2022-07-31 16:45:23 -0400

    solve common_word

--- ch_09/common_word.py
+++ ch_09/common_word.py
@@ -1,21 +1,32 @@
-# def common_word(file):
-#     handle = open(file)
-#     store = []
-#     for line in handle:
-#         # words = line.split()
-#         new_list = line.split()
-#         store.append(new_list[1])
-#     return store
+def common_word(file):
+    # get text from file
+    handle = open(file)
+    store = []
+    for line in handle:
+        store.append(line.split())
+    flattened_store = [element for sublist in store for element in sublist]
+    string = " ".join(flattened_store)
 
-# print(common_word('lorem.txt'))
+    # remove punctuation from text
+    punc = ''',.'"!'''
+    for letter in string:
+        if letter in punc:
+            string = string.replace(letter, "")
 
+    # create histogram of the frequencies of each word
+    fixed = string.lower().split()
+    hist = {}
+    for word in fixed:
+        hist[word] = hist.get(word, 0) + 1
 
-handle = open('lorem.txt')
+    # find which key of hist has the highest value
+    count = 0
+    highest = ''
+    for key in hist:
+        if hist[key] > count:
+            count = hist[key]
+            highest = key
 
-# extract all emails and store in a lis
-# store = []
-for line in handle:
-    line = line.rstrip()
-    print(line)
+    return "the word that appears most often is {highest}; it appears {count} times".format(highest = highest, count = count)
 
-# print(store)
+print(common_word('lorem.txt'))
