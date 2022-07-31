acb4a1d3cc60bc6875e74435bad05da5b69328b8
Evan Jones <evanjones125@gmail.com>
2022-07-31 18:05:37 -0400

    add text file

---
+++ ch_08/ex_8_4.py
@@ -0,0 +1,23 @@
+def sort_remove_duplicates(file):
+    fh = open(file)
+    lst = []
+    for line in fh:
+        lst.append(line.split())
+    
+        
+    flat_list = []
+    for xs in lst:
+        for x in xs:
+            flat_list.append(x)
+        
+    flat_list.sort()
+
+    output = []
+
+    for i in flat_list:
+        if i not in output:
+            output.append(i)
+
+    return output
+
+print(sort_remove_duplicates('romeo.txt'))
