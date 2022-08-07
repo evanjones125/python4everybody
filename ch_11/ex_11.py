c5188498a3862b3b007a12cd8ed23dfb988117fa
Evan Jones <evanjones125@gmail.com>
2022-08-07 16:51:44 -0400

    solve ex_11 with concurrent for loops

--- ch_11/ex_11.py
+++ ch_11/ex_11.py
@@ -1,7 +1,18 @@
+import re
+
 def haystack(file):
     handle = open(file)
     total = 0
+    nums = []
+    
+    for line in handle:
+        nums.append(re.findall('[0-9]+', line))
+
+    flatten_list = [element for sublist in nums for element in sublist]
+
+    for i in flatten_list:
+        total += int(i)
 
     return total
 
-print(haystack('regex_sum_42'))
+print(haystack('regex_sum_42.txt'))
