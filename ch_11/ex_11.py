e2c6c263ec7db8aaaf808569eaf083da9b6f1fbe
Evan Jones <evanjones125@gmail.com>
2022-08-07 16:55:34 -0400

    add pseudocode to ex_11

--- ch_11/ex_11.py
+++ ch_11/ex_11.py
@@ -5,14 +5,15 @@ def haystack(file):
     total = 0
     nums = []
     
+    # add all numbers to list nums
     for line in handle:
         nums.append(re.findall('[0-9]+', line))
 
+    # flatten nums and iterate through to add int value of i to total
     flatten_list = [element for sublist in nums for element in sublist]
-
     for i in flatten_list:
         total += int(i)
 
     return total
 
-print(haystack('regex_sum_42.txt'))
+print(haystack('regex_sum_1585134.txt'))
