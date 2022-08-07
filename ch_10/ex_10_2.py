3c04f5461d2238835de92581874400cdca69faa3
Evan Jones <evanjones125@gmail.com>
2022-08-06 21:12:46 -0400

    add pseudocode to ex_10_2

--- ch_10/ex_10_2.py
+++ ch_10/ex_10_2.py
@@ -3,18 +3,20 @@ def hours(file):
     hours = []
     store = {}
 
-    # if line starts with from, take the two digits prior to the first instance of ':'
+    # take the two digits prior to the first instance of ':' and store in a list
     for line in handle:
         if line.startswith("From "):
             hour = line[line.find(':') - 2] + line[line.find(':') - 1]
             hours.append(hour)
 
+    # populate store dictionary with hours values and their frequencies
     for hour in hours:
         if hour in store:
             store[hour] += 1
         else:
             store[hour] = 1
 
+    # sort store and print each property as a tuple
     for i in sorted(store):
         print(i, store[i])
 
